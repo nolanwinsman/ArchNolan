90c484d42bae8bd9f6c24b24aaa2753bcdc1ab5e
Nolan <68390327+nolanwinsman@users.noreply.github.com>
2022-01-09 11:29:15 -0600

    added more legible echo's

--- ArchNolan.sh
+++ ArchNolan.sh
@@ -6,6 +6,8 @@
 # TODO make script install snap if not present
 # TODO downgrade yay Package
 
+SCRIPTHOME = 'ArchNolan'
+
 PKGS_PACMAN=(
 'discord'
 'telegram-desktop'
@@ -57,47 +59,61 @@ PKGS_VSCODE=(
 )
 
 # installs pacman packages
-echo 'Installing pacman packages'
+echo -ne "
+-------------------------------------------------------------------------
+
+                            Pacman Pakages
+
+-------------------------------------------------------------------------
+"
 for PKG in "${PKGS_PACMAN[@]}"; do
     echo "INSTALLING: ${PKG}"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
 
 # installs snap packages
-echo 'Installing snap packages'
+echo -ne "
+-------------------------------------------------------------------------
+
+                            Snap Pakages
+
+-------------------------------------------------------------------------
+"
 for PKG in "${PKGS_SNAP[@]}"; do
     echo "INSTALLING: ${PKG}"
     sudo snap install "$PKG"
 done
 
-echo 'installing Python modules'
+echo -ne "
+-------------------------------------------------------------------------
+
+                            Python Modules
+
+-------------------------------------------------------------------------
+"
 pip install -r python_modules.txt
 
 # installs VsCode extensions
-echo 'Installing Python Modules'
+echo -ne "
+-------------------------------------------------------------------------
+
+                            VS Code Extensions
+
+-------------------------------------------------------------------------
+"
 for PKG in "${PKGS_VSCODE[@]}"; do
     echo "INSTALLING: ${PKG}"
     code --install-extension ${PKG}
 done
 
-# Installs the newest GloriousEggroll Proton file and places it in the Steam compatability directory
-echo 'Installing GloriousEggroll Proton to Steam directory'
-curl -IkLs -o NUL -w %{url_effective} https://github.com/GloriousEggroll/proton-ge-custom/releases/latest \
-     | grep -o "[^/]*$"\
-     | xargs -I T \
-       curl -kL https://github.com/GloriousEggroll/proton-ge-custom/releases/download/T/Proton-T.tar.gz \
-       -o temp.tar.gz
-tar xf temp.tar.gz
-sudo rm temp.tar.gz
-sudo rm NUL
-
-sudo mkdir "/home/nolan/.steam/root/compatibilitytools.d"
-for f in */ ; do 
-    mv "$f" "/home/nolan/.steam/root/compatibilitytools.d";
-    sudo rm -r "$f"; # used for if the folder is already in compatabilitytools.d
-done
-
 # Graphics Drivers find and install
+echo -ne "
+-------------------------------------------------------------------------
+
+                            Graphics Drivers
+
+-------------------------------------------------------------------------
+"
 gpu_type=$(lspci)
 if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
     pacman -S nvidia --noconfirm --needed
@@ -110,8 +126,16 @@ elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
 fi
 
+echo -ne "
+-------------------------------------------------------------------------
+
+                            Kitty Terminal Setup
+
+-------------------------------------------------------------------------
+"
+
 export PATH=$PATH:~/.local/bin
-cp -r $HOME/$SCRIPTHOME/dotfiles/* $HOME/.config/
+cp -r $HOME/Documents/Github/ArchNolan/dotfiles/* $HOME/.config/
 pip install konsave
 konsave -i $HOME/$SCRIPTHOME/kde.knsv
 sleep 1
@@ -127,8 +151,37 @@ cp -r $HOME/.config/kitty $HOME/$SCRIPTHOME/dotfiles/kitty
 konsave -s kde
 konsave -e kde
 
-# Downloads Bulk Rename Utility and installs it
-echo "Installing Bulk Rename Utility"
-wget https://www.bulkrenameutility.co.uk/Downloads/BRU_setup.exe
-wine BRU_setup.exe # installs Bulk Rename Utility using WINE
-sudo rm BRU_setup.exe
+# # Downloads Bulk Rename Utility and installs it
+# echo -ne "
+# -------------------------------------------------------------------------
+
+#                             Bulk Rename Utility
+
+# -------------------------------------------------------------------------
+# "
+# wget https://www.bulkrenameutility.co.uk/Downloads/BRU_setup.exe
+# wine BRU_setup.exe # installs Bulk Rename Utility using WINE
+# sudo rm BRU_setup.exe
+
+# # Installs the newest GloriousEggroll Proton file and places it in the Steam compatability directory
+# echo -ne "
+# -------------------------------------------------------------------------
+
+#                             Proton-GE
+
+# -------------------------------------------------------------------------
+# "
+# curl -IkLs -o NUL -w %{url_effective} https://github.com/GloriousEggroll/proton-ge-custom/releases/latest \
+#      | grep -o "[^/]*$"\
+#      | xargs -I T \
+#        curl -kL https://github.com/GloriousEggroll/proton-ge-custom/releases/download/T/Proton-T.tar.gz \
+#        -o temp.tar.gz
+# tar xf temp.tar.gz
+# sudo rm temp.tar.gz
+# sudo rm NUL
+
+# sudo mkdir "/home/nolan/.steam/root/compatibilitytools.d"
+# for f in */ ; do 
+#     mv "$f" "/home/nolan/.steam/root/compatibilitytools.d";
+#     sudo rm -r "$f"; # used for if the folder is already in compatabilitytools.d
+# done
