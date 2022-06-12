d8404dc3faf4c37973983d18ca25622b7c208d91
Nolan <nolanwinsman@gmail.com>
2022-06-12 08:06:42 -0600

    Started script for setting up my server

---
+++ server.sh
@@ -0,0 +1,48 @@
+# gets the username from the user for /home/username
+echo "Please input your username:"
+read MAINUSER
+echo "$MAINUSER"
+
+# checks if the directory exists
+if [ -d "/home/$MAINUSER/" ]
+then
+    HOMEDIR="/home/$MAINUSER"
+    echo "$HOMEDIR exists, continuing with program"
+else
+    # directory does not exist so the code is exited
+    echo "/home/$MAINUSER/ does not exist, exiting code"
+    exit 404
+fi
+
+CWD=$(pwd) # sets the current working directory to a variable
+
+
+echo -ne "
+
+----------------------------------------------------------------------
+
+                            Enabling SSH
+
+----------------------------------------------------------------------
+
+"
+
+# function to enable ssh on this machine
+setup_ssh() {
+    sudo pacman -S openssh --noconfirm --needed
+    sudo systemctl start sshd
+    sudo systemctl enable sshd
+    echo "SSH enabled, use the command 'ip a' to find your ip address"
+
+}
+if [ $AUTO == true ]
+then
+    setup_ssh
+else
+    read -p "Do you want to Enable SSH? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        setup_ssh
+    fi
+fi
